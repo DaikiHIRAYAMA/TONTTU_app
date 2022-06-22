77663df21db2c3a2e640ec7af801aee2b3f07e6e
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-22 11:23:53 +0900

    404 500

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,9 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+
+  %w(get put patch post delete).each do |method|
+    send(method, '*something', controller: :application, action: :render_404)
+  end
+
 end
