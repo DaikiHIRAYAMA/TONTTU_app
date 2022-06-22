5ba24e01bbeee87a3e62483d5d8a85f169614a87
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-22 13:12:30 +0900

    time change to Time

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
 
 
   %w(get put patch post delete).each do |method|
-    send(method, '*something', controller: :application, action: :render_404)
-  end
+  send(method, '*something', controller: :application, action: :render_404)
+end
 
 end
