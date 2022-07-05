0d9188df73009fb0ae7eee9e7aa494b1bc601acc
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-07-05 15:15:27 +0900

    validate plus

--- app/models/sauna.rb
+++ app/models/sauna.rb
@@ -1,4 +1,13 @@
 class Sauna < ApplicationRecord
     has_and_belongs_to_many :timers
     belongs_to :user, optional: true
+
+    validates :sauna_temperature, numericality: {greater_than: 0,less_than: 200}
+
+    validates :sauna_humidity, numericality: {greater_than: 0,less_than: 200}
+
+    validates :water_temperature, numericality: {greater_than: -50,less_than: 50}
+
+
+
 end
